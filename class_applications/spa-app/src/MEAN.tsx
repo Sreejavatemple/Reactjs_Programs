446dc7505a58c8f3b1561a6db4223ebfbc43ab46
Srikanth-786-Sree <Sreejavatemple>
2020-12-28 19:00:30 +0530

    state-app status chenged

--- class_applications/spa-app/src/MEAN.tsx
+++ class_applications/spa-app/src/MEAN.tsx
@@ -3,7 +3,7 @@ import React,{Component} from "react";
 export default class MEAN extends React.Component{
     render():any{
         return(
-            <h1>WELCOME TO MEAN..............!</h1>
+            <h1>WELCOME TO MEAN....................!</h1>
         )
     }
 }
